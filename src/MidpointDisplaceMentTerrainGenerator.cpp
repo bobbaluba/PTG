5e127eb2089b8693934844ad3c110b384a6960ba
Johan Helsing <bobbaluba@gmail.com>
2012-10-23 16:00:35 +1100

    Stubs for midpoint displacement algorithm

---
+++ src/MidpointDisplaceMentTerrainGenerator.cpp
@@ -0,0 +1,21 @@
+/*
+ * MidpointDisplaceMentTerrainGenerator.cpp
+ *
+ *  Created on: 23. okt. 2012
+ *      Author: johan
+ */
+
+#include "/mnt/shared/johan/repos/PTG/src/MidpointDisplaceMentTerrainGenerator.h"
+
+namespace ptg {
+
+MidpointDisplaceMentTerrainGenerator::MidpointDisplaceMentTerrainGenerator() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MidpointDisplaceMentTerrainGenerator::~MidpointDisplaceMentTerrainGenerator() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace ptg */
---
+++ src/MidpointDisplaceMentTerrainGenerator.h
@@ -0,0 +1,49 @@
+/*
+ * MidpointDisplaceMentTerrainGenerator.h
+ *
+ * @date 2012
+ * @author Johan Helsing
+ *
+ */
+
+#ifndef MIDPOINTDISPLACEMENTTERRAINGENERATOR_H_
+#define MIDPOINTDISPLACEMENTTERRAINGENERATOR_H_
+
+#include "TerrainGenerator.h"
+
+namespace ptg {
+
+/** @brief Generates terrain using the midpoint displacement algorithm
+ *
+ * Algorithm
+ * ---------
+ * Recur
+ *
+ * Memory footprint
+ * ----------------
+ * The algorithm has an extremely small memory footprint, only a single number is needed
+ * to seed the random generator.
+ *
+ * Complexity
+ * ----------
+ * All gridpoints are visited exactly once, each point use constant time, making the complexity
+ *
+ *     O(WIDTH^2)
+ *
+ * Limitations and Artifacts
+ * -------------------------
+ * The algorithm creates obvious artifacts in a square like pattern.
+ *
+ * The width of the terrain has to be 2^n+1 where n is a positive integer
+ *
+ * Unless a bi-directional pseudo-random number generator is used,
+ * the maximum level of detail or the maximum area the terrain covers are bounded.
+ */
+class MidpointDisplaceMentTerrainGenerator: public TerrainGenerator {
+public:
+	MidpointDisplaceMentTerrainGenerator();
+	virtual ~MidpointDisplaceMentTerrainGenerator();
+};
+
+} /* namespace ptg */
+#endif /* MIDPOINTDISPLACEMENTTERRAINGENERATOR_H_ */
