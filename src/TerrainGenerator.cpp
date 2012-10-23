333ca70c9e1bfd9fac74b772edc761181cfe62f4
Johan Helsing <bobbaluba@gmail.com>
2012-10-23 15:24:02 +1100

    TerrainGenerator stub

---
+++ src/TerrainGenerator.cpp
@@ -0,0 +1,18 @@
+/*
+ * TerrainGenerator.cpp
+ *
+ *  Created on: 23. okt. 2012
+ *      Author: johan
+ */
+
+#include "TerrainGenerator.h"
+
+TerrainGenerator::TerrainGenerator() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TerrainGenerator::~TerrainGenerator() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/TerrainGenerator.h
@@ -0,0 +1,19 @@
+/*
+ * TerrainGenerator.h
+ *
+ *  Created on: 23. okt. 2012
+ *      Author: johan
+ */
+
+#ifndef TERRAINGENERATOR_H_
+#define TERRAINGENERATOR_H_
+
+#include "Seedable.h"
+
+class TerrainGenerator: public Seedable {
+public:
+	TerrainGenerator();
+	virtual ~TerrainGenerator();
+};
+
+#endif /* TERRAINGENERATOR_H_ */
