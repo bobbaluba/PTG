be0a634dbef6e6298c320a49f79c47faddf55851
Johan Helsing <bobbaluba@gmail.com>
2012-10-31 10:28:44 +1100

    Documentation for Continuous2dSignalTerrain

--- include/ptg/Continuous2DSignalTerrain.hpp
+++ include/ptg/Continuous2DSignalTerrain.hpp
@@ -1,30 +1,38 @@
 /*
  * Continuous2DSignalTerrain.hpp
  *
- *  Created on: 26. okt. 2012
- *      Author: Johan Klokkhammer Helsing
+ * @date 26. okt. 2012
+ * @author Johan Klokkhammer Helsing
  */
 
-#ifndef CONTINUOUS2DSIGNALTERRAIN_H_
-#define CONTINUOUS2DSIGNALTERRAIN_H_
+#ifndef CONTINUOUS2DSIGNALTERRAIN_HPP
+#define CONTINUOUS2DSIGNALTERRAIN_HPP
 
 #include <ptg/Terrain.hpp>
 #include <ptg/Continuous2DSignal.hpp>
 
 namespace ptg {
 
+/**
+ * @brief Terrain using a Continuous2DSignal as source
+ */
 class Continuous2DSignalTerrain: public ptg::Terrain {
 public:
 	Continuous2DSignalTerrain(unsigned int seed=0);
 	virtual ~Continuous2DSignalTerrain();
+
+	///@brief Set source signal of the terrain
 	virtual void setSignal(Continuous2DSignal* signal);
+
 	virtual helsing::HeightMap generateHeightMap(unsigned int gridPoints, float gridSpacing);
 	virtual void onReSeed(unsigned int seed);
+
+	///@brief Set the amplitude of the terrain
 	virtual void setAmplitude(float amplitude){this->amplitude=amplitude;}
 private:
 	Continuous2DSignal* signal;
 	float amplitude;
 };
 
-} /* namespace ptg */
-#endif /* CONTINUOUS2DSIGNALTERRAIN_H_ */
+} // namespace ptg
+#endif // CONTINUOUS2DSIGNALTERRAIN_HPP
