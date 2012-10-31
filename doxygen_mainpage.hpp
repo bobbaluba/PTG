be101e0571150953d20fc447689e2bb2ca889c23
Johan Helsing <bobbaluba@gmail.com>
2012-10-31 15:15:05 +1100

    Added a mainpage to doxygen documentation

---
+++ doxygen_mainpage.hpp
@@ -0,0 +1,10 @@
+/**
+ * @mainpage PTG Index
+ *
+ * A procedural terrain generator.
+ *
+ * Here you can find the API documentation
+ *
+ * see readme for instructions on building/usage of ptgview
+ *
+ */
