0cb476ef189aae0f2112c09b4e480ce4a77483d5
Johan Helsing <bobbaluba@gmail.com>
2012-11-11 16:57:24 +1100

    Fixed bug where diffuse component could be below zero

--- data/shaders/terrain.vert.glsl
+++ data/shaders/terrain.vert.glsl
@@ -11,11 +11,11 @@ out vec3 ex_Color;
 
 void main(void){
 	vec3 n = in_Normal.xyz;
-	vec3 v = vec3(0,0,1); //direction towards viewer
+	vec3 s = normalize(vec3(1,1,1)); //direction towards light source
 	gl_Position = ProjectionMatrix * ModelViewMatrix * in_Position;
 	float k_a = 0.2;
 	float k_d = 0.5;
 	float i_a = k_a;
-	float i_d = k_d * dot(n, v);
+	float i_d = k_d * max(0, dot(n, s));
 	ex_Color = (i_a + i_d) * vec3(1,1,1);
 }
