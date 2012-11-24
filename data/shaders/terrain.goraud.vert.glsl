a12b064f630aee85161964d833dddea61e25d603
Johan Helsing <bobbaluba@gmail.com>
2012-11-24 12:10:47 +0100

    Specular lighting for goraud shader

--- data/shaders/terrain.goraud.vert.glsl
+++ data/shaders/terrain.goraud.vert.glsl
@@ -10,12 +10,19 @@ in vec4 in_Normal;
 out vec3 ex_Color;
 
 void main(void){
-	vec3 n = in_Normal.xyz;
-	vec3 s = normalize(vec3(1,1,1)); //direction towards light source
+	mat4 MVP  = ProjectionMatrix * ModelViewMatrix;
+
+	vec3 n = normalize(ModelViewMatrix * in_Normal).xyz; //switch to normal matrix?
+	vec3 s = normalize(ModelViewMatrix * vec4(1,1,1,0)).xyz; //direction towards light source
+	vec3 v = vec3(0,0,1); //direction towards viewer
+	vec3 r = reflect(s, n); //reflection
+	
 	gl_Position = ProjectionMatrix * ModelViewMatrix * in_Position;
 	float k_a = 0.2;
 	float k_d = 0.5;
+	float k_s = 0.2;
 	float i_a = k_a;
 	float i_d = k_d * max(0, dot(n, s));
-	ex_Color = (i_a + i_d) * vec3(1,1,1);
+	float i_s = k_s * max(0, pow(dot(r, v), 50) );
+	ex_Color = (i_a + i_d + i_s) * vec3(1,1,1);
 }
