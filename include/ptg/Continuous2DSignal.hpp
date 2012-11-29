8ee213ab2156352d3c53797f7fcf0cb165eab879
Johan Helsing <bobbaluba@gmail.com>
2012-11-29 13:06:20 +0100

    Additional documentation in Continuous2DSignal

--- include/ptg/Continuous2DSignal.hpp
+++ include/ptg/Continuous2DSignal.hpp
@@ -15,6 +15,15 @@ namespace ptg {
  *
  * Any x or y should return a value.
  * Points with close x and y values, should have close output values
+ * 
+ * Useful in implicit procedural modeling techniques where the procedure answers
+ * a query about a particular point.
+ * 
+ * This class inherits from Seedable because, most frequently, the signal will be
+ * a noise function, or at least based on a noise function. I could have taken an
+ * intermediate step, and created a subclass named Continuous2DNoise, or renamed
+ * the class, but this would only increase code complexity, or create confusion
+ * about 2DSignals that aren't noise.
  */
 class Continuous2DSignal: public Seedable {
 public:
